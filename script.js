90a0d8368e43cae287514b61b6074d04ffdc4214
Shaheen M <115266487+shaheenoz@users.noreply.github.com>
2024-08-28 05:41:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,20 @@ menuIcon.onclick = () =>{
     navbar.classList.toggle('active');
 }
 
+
+ const scriptURL = 'https://script.google.com/macros/s/AKfycby3MhipbMBVL_g6xWjzyjDLOOs2uBFtCTavipaDKia4qix6MiBAUEnO8uqwc9q9qWxR/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully ✔"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
